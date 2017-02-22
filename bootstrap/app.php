512a0b3a68b0fe4d0447ae76a452361d5e9ae50a
BMNTeam <maksim_bender08@mail.ru>
2017-02-22 22:45:50 +0300

    Before working with datatables and database

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,9 @@
 |
 */
 
-$app = new Illuminate\Foundation\Application(
-    realpath(__DIR__.'/../')
-);
+    $app = new Illuminate\Foundation\Application(
+        realpath(__DIR__.'/../')
+    );
 
 /*
 |--------------------------------------------------------------------------
--- resources/views/includes/content.blade.php
+++ resources/views/includes/content.blade.php
@@ -4,11 +4,11 @@
 
                 <div class="row">
 
-                    <div class="col-md-3 fix-right-padding">
+                    <div class="col-md-2 fix-right-padding">
                         @include('includes.menu')
                     </div>
 
-                    <div class="col-md-9 container-background">
+                    <div class="col-md-10 container-background">
                         <div class="content">
                             @yield('content')
                         </div>
