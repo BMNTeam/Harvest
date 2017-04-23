22a75e19a86834c63b43bcd76a1a6edc53378539
BMNTeam <maksim_bender08@mail.ru>
2017-04-23 14:38:03 +0300

    Dump database every minute using default heroky functionality

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // example $schedule->command('inspire')
+        // example1 $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('heroku pg:backups:capture --app harvest-sniish')
                      ->everyMinute();
