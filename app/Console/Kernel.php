19edb347f4a83a4408d03b8ec964d8be9aa8dc1a
BMNTeam <maksim_bender08@mail.ru>
2017-04-23 16:13:10 +0300

    Removed some garbage

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // example $schedule->command('inspire')
-        //          ->hourly();
+        //         ->hourly();
     }
 
     /**
